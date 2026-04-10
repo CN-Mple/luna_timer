4115c14ba22952aa5a7231dcfa5a812caab52a94
Purple Moon <2434717958@qq.com>
2026-04-10 11:16:36 +0800

    Add files via upload

--- luna_timer.h
+++ luna_timer.h
@@ -6,7 +6,7 @@
 
 struct core_timer;
 
-typedef unsigned long   tick_type_t;
+typedef size_t                          tick_type_t;
 typedef void (*luna_timer_callback_t)(struct core_timer *timer);
 
 #ifndef LUNA_GET_TICK
@@ -24,6 +24,10 @@ typedef void (*luna_timer_callback_t)(struct core_timer *timer);
 #define LUNA_EXPIRED                    luna_expired
 #endif
 
+#ifndef LUNA_LESS_THAN
+#define LUNA_LESS_THAN(type, a, b)      LUNA_EXPIRED(type, (a) - (b))
+#endif
+
 struct core_timer {
     struct core_timer *next;
     tick_type_t when;
@@ -33,20 +37,12 @@ struct core_timer {
 void luna_timer_append(struct core_timer **head, struct core_timer *timer);
 void luna_timer_remove(struct core_timer **head, struct core_timer *timer);
 
-tick_type_t luna_timer_get_next_expiry(const struct core_timer *head);
-tick_type_t luna_timer_expired(struct core_timer **head);
+tick_type_t luna_timer_get_next_expiry(struct core_timer **head);
+tick_type_t luna_timer_run(struct core_timer **head);
 
 #endif
 
 #ifdef LUNA_TIMER_IMPLEMENTATION
-
-static bool less_than(const struct core_timer *a, const struct core_timer *b)
-{
-        if (a->when <= b->when) {
-                return true;
-        }
-        return false;
-}
 
 void luna_timer_append(struct core_timer **head, struct core_timer *timer)
 {
@@ -54,7 +50,9 @@ void luna_timer_append(struct core_timer **head, struct core_timer *timer)
         LUNA_ASSERT(timer);
 
         struct core_timer **next = head;
-        while (*next && less_than(*next, timer)) {
+        tick_type_t when = timer->when;
+
+        while (*next && LUNA_LESS_THAN(tick_type_t, (*next)->when, when)) {
                 next = &((*next)->next);
         }
         timer->next = *next;
@@ -77,37 +75,34 @@ void luna_timer_remove(struct core_timer **head, struct core_timer *timer)
         }
 }
 
-tick_type_t luna_timer_get_next_expiry(const struct core_timer *head)
+tick_type_t luna_timer_get_next_expiry(struct core_timer **head)
 {
-        tick_type_t now;
-        tick_type_t diff;
-
         if (!head) {
-                diff = -1;
-                return diff;
+                return (tick_type_t)-1;
         }
-        now = LUNA_GET_TICK();
-        diff = head->when - now;
-        //     A------B
-        // ----B      A---
-        if (luna_expired(tick_type_t, diff)) {
-                diff = 0;
+        tick_type_t now  = LUNA_GET_TICK();
+        tick_type_t when = (*head)->when;
+        if (LUNA_LESS_THAN(tick_type_t, when, now)) {
+                return 0;
         }
-        return diff;
+
+        return when - now;
 }
 
-tick_type_t luna_timer_expired(struct core_timer **head)
+tick_type_t luna_timer_run(struct core_timer **head)
 {
         LUNA_ASSERT(head);
+        tick_type_t next_expiry;
 
-        tick_type_t next = luna_timer_get_next_expiry(*head);
-        if (0 != next) {
-                return next;
+        next_expiry = luna_timer_get_next_expiry(head);
+        if (0 == next_expiry) {
+               struct core_timer *timer = *head;
+                *head = timer->next;
+                if (timer->callback) {
+                        timer->callback(timer);
+                }
         }
-        struct core_timer *timer = *head;
-        *head = timer->next;
-        timer->callback(timer);
-        return 0;
+        return next_expiry;
 }
 
 #endif
